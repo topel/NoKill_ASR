2f685edfd197537c36b232b0954cbd11ef309ef8
ErwanGateauMagdeleine <45492636+ErwanGateauMagdeleine@users.noreply.github.com>
2019-09-09 10:44:31 +0200

    Add files via upload

---
+++ Google_API/ASR_Google_stream.py
@@ -0,0 +1,186 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 17 13:31:35 2019
+
+@author: erwan
+"""
+
+
+import re
+import sys
+import os
+import pyaudio
+import webrtcvad
+from six.moves import queue
+from google.cloud import speech
+from google.cloud.speech import enums
+from google.cloud.speech import types
+
+#Recording parameters
+RATE = 16000
+CHUNK = int(RATE / 33.33)  # 30ms
+
+
+def callback(in_data, frame_count, time_info, status):
+    return (in_data, pyaudio.paContinue)
+
+
+class Audiostream(object):
+    def __init__(self,SAMPLING_RATE,CHUNK):
+        self.sample_rate=SAMPLING_RATE
+        self.chunk=CHUNK
+        # Creation dun buffer pour stocker les echantillons
+        self.buff = queue.Queue()
+        # Set d'une variable pour monitorer l'activation ou non du stream audio
+        self.close = True
+        
+        # Creation de la VAD pour le record (sensibilité max)
+        self.VAD = webrtcvad.Vad()
+        self.VAD.set_mode(3)
+        
+    def __exit__(self,type, value, traceback):
+        self.stream.stop_stream()
+        self.stream.close()
+        
+        self.buff.put(None)
+        self.p.terminate()
+        self.close = True
+        
+    def __enter__(self):
+        self.p = pyaudio.PyAudio()
+        
+        self.stream = self.p.open(format=pyaudio.paInt16,
+                channels=1,
+                rate=self.sample_rate,
+                input=True,
+                output=True, #Desactive l'écoute en temps réel
+                frames_per_buffer=self.chunk,
+                stream_callback=self.callback)
+        self.close = False
+        
+        self.stream.start_stream()
+        
+        return self
+        
+    def callback(self,in_data, frame_count, time_info, flag):
+        if self.VAD.is_speech(in_data,RATE):
+            self.buff.put(in_data)
+            return in_data, pyaudio.paContinue
+        else:
+            return in_data, pyaudio.paContinue
+    
+    def generateur(self):
+        while not self.close:
+            #recuperation d'un echantillon
+            chunk=self.buff.get()
+
+            
+            #quitter la fonction si il n'y a pas d'échantillon
+            if chunk is None:
+                return
+            
+            data=[chunk]
+            
+            #on récupère tous les échantillons possible si ils sont disponibles (sans bloquer)
+            #sinon on arrête la prediction
+            while True:
+                try:
+                    chunk = self.buff.get(block=False)
+                    if chunk is None:
+                        return
+                    data.append(chunk)
+                except queue.Empty:
+                    break
+                
+            yield b''.join(data)
+                        
+            
+def listen_print_loop(responses):
+    """Iterates through server responses and prints them.
+
+    The responses passed is a generator that will block until a response
+    is provided by the server.
+
+    Each response may contain multiple results, and each result may contain
+    multiple alternatives; for details, see https://goo.gl/tjCPAU.  Here we
+    print only the transcription for the top alternative of the top result.
+
+    In this case, responses are provided for interim results as well. If the
+    response is an interim one, print a line feed at the end of it, to allow
+    the next result to overwrite it, until the response is a final one. For the
+    final one, print a newline to preserve the finalized transcription.
+    """
+    num_chars_printed = 0
+    for response in responses:
+        if not response.results:
+            continue
+
+        # The `results` list is consecutive. For streaming, we only care about
+        # the first result being considered, since once it's `is_final`, it
+        # moves on to considering the next utterance.
+        result = response.results[0]
+        if not result.alternatives:
+            continue
+
+        # Display the transcription of the top alternative.
+        transcript = result.alternatives[0].transcript
+
+        # Display interim results, but with a carriage return at the end of the
+        # line, so subsequent lines will overwrite them.
+        #
+        # If the previous result was longer than this one, we need to print
+        # some extra spaces to overwrite the previous result
+        overwrite_chars = ' ' * (num_chars_printed - len(transcript))
+
+        if not result.is_final:
+            sys.stdout.write(transcript + overwrite_chars + '\r')
+            sys.stdout.flush()
+
+            num_chars_printed = len(transcript)
+
+        else:
+            print(transcript + overwrite_chars,'\n')
+
+            # Exit recognition if any of the transcribed phrases could be
+            # one of our keywords.
+            if re.search(r'\b(quitter|quitté)\b', transcript, re.I):
+                print('Exiting..')
+                break
+
+            num_chars_printed = 0
+
+            
+        
+
+####### MAIN #######
+
+# Initialisation Google API
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"]=r"NoKillSpeechRecognition-d838c0d75533.json"
+
+client = speech.SpeechClient()
+
+config = types.RecognitionConfig(
+        encoding=enums.RecognitionConfig.AudioEncoding.LINEAR16,
+        sample_rate_hertz=RATE,
+        language_code='fr-FR')
+
+streaming_config = types.StreamingRecognitionConfig(
+        config=config,
+        interim_results=True)
+
+
+
+with Audiostream(RATE,CHUNK) as stre:
+        audio_gen =  stre.generateur()
+
+#        for content in audio_gen:
+#            print(content)
+
+        requests = (types.StreamingRecognizeRequest(audio_content=content)
+                  for content in audio_gen)
+        
+        responses = client.streaming_recognize(streaming_config, requests)
+        
+        listen_print_loop(responses)
+
+print("exited")
---
+++ Google_API/ASR_Google_synchronous.py
@@ -0,0 +1,151 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun  6 09:48:54 2019
+@author: erwan Gateau-Magdeleine
+IRIT SAMOVA
+
+Scrip permettant la reconnaissance automatique de parole en utilisant l'API google pour des fichiers courts (<1min)
+Le fihier doit être au format .wav Il peut être stereo, dans ce cas, il sera transformé en mono par l'algorithme
+
+"""
+import os
+import sys
+import io
+from pydub import AudioSegment
+import wave
+
+
+
+def frame_rate_channel(audio_file_name):
+    # Récupere la frequence d'echantillonage et le nombre de canaux
+    with wave.open(audio_file_name, "rb") as wave_file:
+        frame_rate = wave_file.getframerate()
+        channels = wave_file.getnchannels()
+    return frame_rate,channels
+
+def stereo_to_mono(audio_file_name):
+    # Transforme un son stereo en un son mono
+    sound = AudioSegment.from_wav(audio_file_name)
+    sound = sound.set_channels(1)
+    sound.export(audio_file_name, format="wav")
+  
+def transcribe_file_multiple_speakers(speech_file):
+    '''
+    Permet la transcription d'un fichier audio en texte en utilisant un process synchrone
+    Le fichier doit être < 1min
+    '''
+    trans = ''
+    conf = []
+    frame_rate,channels = frame_rate_channel(speech_file)
+    
+    # Si plus de 1 cannal dans le fichier audio
+    if channels > 1:
+        stereo_to_mono(speech_file)
+        
+    #from google.cloud import speech
+    #from google.cloud.speech import enums
+    #from google.cloud.speech import types
+    
+    from google.cloud import speech_v1p1beta1 as speech
+    
+    client = speech.SpeechClient()
+    
+    with io.open(speech_file,'rb') as audio_file:
+         content = audio_file.read() #permet d'avoir un fichier binaire en bytes
+         
+    audio = speech.types.RecognitionAudio(content=content)
+    config = speech.types.RecognitionConfig(
+        encoding=speech.enums.RecognitionConfig.AudioEncoding.LINEAR16,
+        sample_rate_hertz=frame_rate,
+        enable_speaker_diarization=True, # Permet d'activer l'option de plusieurs speakers
+        diarization_speaker_count=2, # donne le nombre de sources differentes
+        #enable_automatic_punctuation=True, # Active la ponctuation automatique : ne fonctionne pas!!
+        language_code='fr-FR')
+    
+    response = client.recognize(config, audio)
+    for result in response.results:
+        # The first alternative is the most likely one for this portion.
+        trans += result.alternatives[0].transcript
+        conf.append(result.alternatives[0].confidence)
+    
+    words_info = result.alternatives[0].words
+    for word_info in words_info:
+        print("word: '{}', speaker_tag: {}".format(word_info.word, word_info.speaker_tag))
+    
+    return trans,conf
+
+def transcribe_file_simple_speaker(speech_file):
+    '''
+    Permet la transcription d'un fichier audio en texte en utilisant un process synchrone
+    Le fichier audio doit être < 1min
+    '''
+    trans = ''
+    conf = []
+    frame_rate,channels = frame_rate_channel(speech_file)
+    
+    # Si plus de 1 cannal dans le fichier audio
+    if channels > 1:
+        stereo_to_mono(speech_file)
+        
+    from google.cloud import speech
+    from google.cloud.speech import enums
+    from google.cloud.speech import types
+        
+    client = speech.SpeechClient()
+    
+    with io.open(speech_file,'rb') as audio_file:
+         content = audio_file.read() #permet d'avoir un fichier binaire en bytes
+         
+    audio = speech.types.RecognitionAudio(content=content)
+    config = types.RecognitionConfig(
+        encoding=enums.RecognitionConfig.AudioEncoding.LINEAR16,
+        sample_rate_hertz=frame_rate,
+        use_enhanced = True, 
+        #enable_automatic_punctuation=True, # Active la ponctuation automatique : ne fonctionne pas en français!!
+        language_code='fr-FR',
+        #use_enhanced=True,
+        #model = 'command_and_search'
+        #speech_contexts=[speech.types.SpeechContext(phrases=['quel sens l''IA va bouleverser','liés aux progrès de L''IA à envisager','Turing','Test'])]
+	)
+    
+    response = client.recognize(config, audio)
+    
+    #response = operation.result()
+    
+    for result in response.results:
+        # The first alternative is the most likely one for this portion.
+        trans += result.alternatives[0].transcript
+        conf.append(result.alternatives[0].confidence)
+    
+    return trans,conf
+
+def ecriture_transcription(trans_filename,trans):
+    f= open(trans_filename,"w+")
+    f.write(trans)
+    f.close()
+    
+    
+if len(sys.argv) != 4:
+    print("mauvaise utilisation !!")
+    print("utilisation :")
+    print("python3 ASR_synchronous.py <file_name> <out_path> <transcription name>")
+    sys.exit()
+else:
+    file_name = sys.argv[1]
+    print(file_name)
+    output_path =sys.argv[2]
+    trans_name=sys.argv[3]
+
+# Definition de la variable d'environnement GOOGLE_APPLICATION_CREDENTIALS
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="/home/pellegri/tools/ASR_DEMO_SAMOVA/Stage_Erwan/Google_API/NoKillSpeechRecognition-d838c0d75533.json"
+
+# Transcription du texte en utilisant l'API GOOGLE
+transcription,confidence = transcribe_file_simple_speaker(file_name)
+
+# Ecriture de la transcription dans un fichier
+
+ecriture_transcription(output_path+trans_name,transcription)
+
+# Affichage de la transcription
+print(transcription)
+
